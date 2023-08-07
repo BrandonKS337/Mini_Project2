0aaff6bec572853d9ecaaacdf567b23d3cfedc0e
Your Name <brandonks337@gmail.com>
2023-08-07 14:02:22 -0500

    base express app with routes, models/schema, controllers and the base express app root files setup. After I finish up some other work I'll expand the schema/models section to create more tables for input. I need atleast 1 more to take in characters that the players will be building or picking from. Another for monsters would be good because with atleast those three tables I can create the 3 main components of my mvp

---
+++ README.md
@@ -0,0 +1,9 @@
+this is the second mini exercise/project. We chose to orient this around the capstone and focus on creating the backend server and its connections. The goal personally is to update at least the user login to meet the minimum requirements of CRUD design.
+
+How to run this
+ - pull dependencies with npm i
+ - create a schema for mysql
+ - set env info for mysql server
+ - run "npm run server" // alt npm run dev depending on deployment choice
+
+ should fire up so long as those base steps are taken
---
+++ routes/userRoutes.js
@@ -0,0 +1,47 @@
+//setting up the routes to be used for trafficking new data
+
+const express = require("express");
+const router = express.Router();
+const Controllers = require("../controllers");
+
+//route sudo notes here for each route to help with testing or writing.
+
+//localhost: 8000/api/users <<-- base route
+
+//localhost: 8000/api/users/
+router.get("/", (req, res) => {
+  Controllers.userController.getUsers(res);
+  /*
+    1: looks inside our controllers folder because of line 7
+    2: looks inside our useController.js
+    3: references the getUsers function in useController.js and pulls in the response data
+    */
+});
+//localhost:8000/api/users/:<user_id>
+router.get("/:id", (req, res) => {
+  Controllers.userController.getUsersById(req, res);
+});
+
+//localhost:8000/api/users/create
+router.post("/create", (req, res) => {
+  Controllers.userController.createUsers(req.body, res);
+});
+
+//localhost:8000/api/users/login
+router.post("/login", (req, res) => {
+  Controllers.userController.getUsersByEmail(req, res);
+});
+
+//don't worry about login in our app
+
+//localhost:8000/api/users/:<user_id>
+router.put("/:id", (req, res) => {
+  Controllers.userController.updateUser(req, res);
+});
+
+//localhost:8000/api/users/:<user_id>
+router.delete("/:id", (req, res) => {
+  Controllers.userController.deleteUser(req, res);
+});
+
+module.exports = router;
